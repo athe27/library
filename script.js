f0b6771a1c3918802d0256d82cb016ea6aed9a19
bluediamondshield <tonyyangt123@gmail.com>
2022-11-03 18:39:06 -0400

    finished up javascript, added a remove and add book function, also made colors save to identify the book

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-let myLibrary = [1, 2, 3];
+let myLibrary = [];
 const bookDescription = document.querySelector(".description")
+let formDisplayed = false
+const submitButton = document.querySelector("#submit-button")
+const form = document.querySelector("form")
+const addBookButton = document.querySelector(".add-book")
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault()
+    if (form.title.value === "" || form.author.value === "" || form.pages.value < 1) {
+        return
+    }
+
+    const book = new Book(form.title.value, form.author.value, form.pages.value, form.read.checked)
+    addBookToLibrary(book)
+    displayBooks()
+    formDisplayed = false
+    //form.style.display = "none"
+    //form.reset()
+})
+
+addBookButton.addEventListener("click", (event) => {
+    if (formDisplayed) {
+        formDisplayed = false
+        form.style.display = "none"
+    } else {
+        formDisplayed = true
+        form.style.display = "block"
+    }
+})
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -25,13 +52,17 @@ function addBookToLibrary(book) {
 function displayBooks() {
     let row = 0;
     const shelves = document.querySelector(".container").children;
+    for (let i = 0; i < shelves.length; i++) {
+        shelves[i].innerHTML = ""
+    }
     for (let i = 0; i < myLibrary.length; i++) {
         if (i > 0 && i % 20 == 0) {
             row++
         }
         const book = document.createElement("div")
+        book.dataset.index = i
         book.classList.add("book")
-        book.style.backgroundColor = generateRandomColor()
+        book.style.backgroundColor = myLibrary[i].color
         book.addEventListener("mouseover", (event) => {
             bookDescription.style.display = "block";
             let offsets  = book.getBoundingClientRect();
@@ -40,9 +71,19 @@ function displayBooks() {
             bookDescription.style["z-index"] = "2"
             bookDescription.style.left = `${x}px`
             bookDescription.style.top = `${y}px`
+            const descriptionElements = bookDescription.children
+            descriptionElements.item(0).innerHTML = "Title: " + myLibrary[i].title
+            descriptionElements.item(1).innerHTML = "Author: " + myLibrary[i].author
+            descriptionElements.item(2).innerHTML = "Pages: " + myLibrary[i].pages
+            descriptionElements.item(3).innerHTML = "Read: " + myLibrary[i].read
         })
         book.addEventListener("mouseout", (event) => {
             bookDescription.style.display = "none";
+        })
+        book.addEventListener("click", (event) => {
+            myLibrary.splice(i, 1)
+            bookDescription.style.display = "none";
+            displayBooks()
         })
         shelves.item(row).appendChild(book)
     }
